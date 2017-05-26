99e35d9f7b03a81da07749c22c0658a3d095fbd8
marlonprudente <marlonmateuspr@gmail.com>
2017-05-26 04:23:48 -0300

    Join quase funcionando

---
+++ 08_Operador_Join/datatypes.h
@@ -0,0 +1,73 @@
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DAINF UTFPR
+// Versão 1.0 -- Março de 2015
+//
+// Estruturas de dados internas do sistema operacional
+
+#ifndef __DATATYPES__
+#define __DATATYPES__
+
+#include <ucontext.h>
+#include "queue.h"
+
+//Estado de uma tarefa (conforme diagrama de estados): Nova, Pronta, Suspensa e Terminada.
+enum status_t{NOVO, PRONTO, EXECUTANDO, SUSPENSO, FINALIZADO};
+
+//Estrutura com a informação do dono da tarefa, se é do usuário ou do sistema, para implementar o quantum
+typedef enum task_dono{ USUARIO, SISTEMA
+}task_dono_t;
+
+typedef unsigned int sys_clock_t;
+typedef unsigned long long count_t;
+typedef unsigned char bool;
+
+// Estrutura que define uma tarefa
+typedef struct task_t
+{
+    struct task_t *prev;    //Próxima tarefa da fila
+    struct task_t *next;    //Tarefa anterior da fila
+    int id;                //Id da tarefa
+    ucontext_t context;     //Contexto da tarefa
+    enum status_t status;   //Estado da tarefa
+    struct task_t *parent;  //"Pai" da tarefa (tarefa em execução quando esta tarefa foi criada)
+    struct queue_t **fila_atual;
+    int prio_estat;
+    int prio_dinam;
+
+    task_dono_t task_dono; //De quem é a tarefa, do Usuário ou do sistema, para controle do quantum
+
+    sys_clock_t t_inicio;
+    sys_clock_t t_executado;
+    count_t contador_processo;
+
+    int ex_status;
+    struct  task *fila_taguardando;
+    bool lock_p;
+
+} task_t ;
+
+// estrutura que define um semáforo
+typedef struct
+{
+  // preencher quando necessário
+} semaphore_t ;
+
+// estrutura que define um mutex
+typedef struct
+{
+  // preencher quando necessário
+} mutex_t ;
+
+// estrutura que define uma barreira
+typedef struct
+{
+  // preencher quando necessário
+} barrier_t ;
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+  // preencher quando necessário
+} mqueue_t ;
+
+#endif
